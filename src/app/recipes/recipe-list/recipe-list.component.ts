af94cb8034633f9a58eb725a152f60f284af6f8c
me.ghafari <me.ghafari@ME-GHAFARI>
2022-04-17 09:10:27 +0430

    Add header

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  reciepes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'C:\Users\me.ghafari\Downloads\Wallpapers\Cute-Dog-White1.jpg')
+  ];
 
   constructor() { }
 
