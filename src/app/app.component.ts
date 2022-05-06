0b743f59661a3fc7bde4f2380325973bad7aaaa5
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-05-06 15:13:17 +0430

    Arrange headers link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
 
+  onNavigate(fearure: string){
+    this.loadedFeature = fearure;
+  }
 }
