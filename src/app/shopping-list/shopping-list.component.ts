e1d2e23c0c1e7710a264f0a82a51ec21df0d7c91
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-05-16 20:27:54 +0430

    add slService for shopping lists

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../Shared/Ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,17 +8,12 @@ import { Ingredient } from '../Shared/Ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients : Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
-  ];
+  ingredients!: Ingredient[];
+  
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
-  }
-
-  onIgredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
+    this.ingredients = this.slService.getIngeridients();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
+import { Ingredient } from "../Shared/Ingredient.model";
+
 export class ShoppingListService{
-    
+    ingredients : Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+      ];
+
+      getIngeridients(){
+          return  this.ingredients.slice();
+      }
+
+      addIngeridient(ingredient: Ingredient){
+          this.ingredients.push(ingredient);
+      }
 }
