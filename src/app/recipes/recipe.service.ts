c79081eaaaa88458b2ecd78917135ef9f51ad6f1
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-05-18 20:06:25 +0430

    add parameter to services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../Shared/Ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
 recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('A Test Recipe',
+                   'This is simply a test',
+                   'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+                   [
+                        new Ingredient('maat', 1),
+                        new Ingredient('french fries', 20)
+                   ]),
+        new Recipe('Another Test Recipe',
+                   'This is simply a test', 
+                   'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+                   [
+                        new Ingredient('buns', 2),
+                        new Ingredient('meat', 1)
+                   ])
       ];
 
       getRecipe(){
