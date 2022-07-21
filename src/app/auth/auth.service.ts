08b9760dacf35d54ea5c7fe49b47d9d41ff05543
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-07-21 13:37:35 +0430

    Login error handeling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from 'rxjs';
@@ -21,25 +21,37 @@ export class authService{
             email: email,
             password: password,
             returnSecureToken: true
-        }).pipe(catchError(errorRes => {
-            let errorMessage = 'An error occured';
+        })
+        .pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string){
+        return this.http.post<AuthResponseData>(
+            '',{
+            email: email,
+            password: password,
+            returnSecureToken: true
+            }
+        )
+            .pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse){
+        let errorMessage = 'An error occured';
             if(!errorRes.error || !errorRes.error.error){
                 return throwError(errorMessage);
             }
             switch(errorRes.error.error.message){
                 case 'EMAIL_EXISTS':
                 errorMessage = 'This email exist already';
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = 'this email does not exist';
+                    break;
+                case 'INVALID_PASSWORD':
+                    errorMessage = 'this password is not correct';
+                    break; 
             }
             return throwError(errorMessage);
-        }));
-    }
-
-    login(email: string, password: string){
-        return this.http.post<AuthResponseData>(
-            '',
-            email: email,
-            password: password,
-            returnSecureToken: true
-        );
+        }
     }
 }
