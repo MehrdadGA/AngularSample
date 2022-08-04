964e604ed1125be50d2a731e3629fe62ffe0606d
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-08-04 09:59:14 +0430

    complete AuthInterceptorService class

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,4 +1,14 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { authService } from "./auth.service";
 
 @Injectable()
-export class AuthInterceptorService{}
+export class AuthInterceptorService implements HttpInterceptor{
+    constructor(private authService: authService){}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        this.authService.user.subscribe();
+        return next.handle(req);
+    }
+}
