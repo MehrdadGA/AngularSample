d2640918157bb65cb08d3115db30b979387ea944
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-08-07 13:35:54 +0430

    localStorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -72,6 +72,7 @@ export class authService{
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
       }
     
 
