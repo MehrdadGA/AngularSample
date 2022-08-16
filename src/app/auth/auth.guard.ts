01e2e9c9bf70c90547005b0a1611939b7b50d251
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-08-16 05:41:07 +0430

    Debug Authguard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,21 +1,43 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { map, Observable, take } from "rxjs";
-import { authService } from "./auth.service";
-
-@Injectable({ providedIn: 'root'})
-export class AuthGuard implements CanActivate{
-    constructor(private authService: authService){}
-    canActivate(route: ActivatedRouteSnapshot, 
-                router: RouterStateSnapshot
-    ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
-        return this.authService.user.pipe(take(1),map(user => {
-            const isAuth = !!user;
-            if(isAuth){
-                return true;
-            }
-            return this.router.createUrlTree(['/auth']);
-            console.log("sd"); 
-        }));
+import {
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Router,
+    UrlTree
+  } from '@angular/router';
+  import { Injectable } from '@angular/core';
+  import { Observable } from 'rxjs';
+  import { map, tap, take } from 'rxjs/operators';
+  
+  import { authService } from './auth.service';
+  
+  @Injectable({ providedIn: 'root' })
+  export class AuthGuard implements CanActivate {
+    constructor(private authService: authService, private router: Router) {}
+  
+    canActivate(
+      route: ActivatedRouteSnapshot,
+      router: RouterStateSnapshot
+    ):
+      | boolean
+      | UrlTree
+      | Promise<boolean | UrlTree>
+      | Observable<boolean | UrlTree> {
+      return this.authService.user.pipe(
+        take(1),
+        map(user => {
+          const isAuth = !!user;
+          if (isAuth) {
+            return true;
+          }
+          return this.router.createUrlTree(['/auth']);
+        })
+        // tap(isAuth => {
+        //   if (!isAuth) {
+        //     this.router.navigate(['/auth']);
+        //   }
+        // })
+      );
     }
-}
+  }
+  
