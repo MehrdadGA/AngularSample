d317a3c6236f5dfcf8f3b1ce765ec59327d29348
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-08-17 05:49:25 +0430

    Debug Auth Part

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,21 +1,30 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { take, exhaustMap } from "rxjs/operators";
-import { authService } from "./auth.service";
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpParams
+} from '@angular/common/http';
+import { take, exhaustMap } from 'rxjs/operators';
+import { authService } from './auth.service';
+
 
 @Injectable()
-export class AuthInterceptorService implements HttpInterceptor{
-    constructor(private authService: authService){}
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private authService: authService) {}
 
-    intercept(req: HttpRequest<any>, next: HttpHandler) {
-        this.authService.user.subscribe();
-        
-    return this.authService.user.pipe(take(1), exhaustMap(user => {
-        if(!user){
-            return next.handle(req);
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        if (!user) {
+          return next.handle(req);
         }
-        const modifiedreq = req.clone({params: new HttpParams().set('auth', user.token)}); 
-        return next.handle(modifiedreq);
-    }));
-    }
+        const modifiedReq = req.clone({
+          params: new HttpParams().set('auth', user.token)
+        });
+        return next.handle(modifiedReq);
+      })
+    );
+  }
 }
