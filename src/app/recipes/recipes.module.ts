1b046cea45dc9103303fc90591fb0174d5420fc2
MehrdadGA <mehrdadghafariafshar@gmail.com>
2022-09-12 05:54:42 +0430

    use with multiple modules part 1

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "../shopping-list/app-routing.module";
+
+@NgModule({
+    declarations:[
+        RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+    
+    ]
+})
+export class RecipeModule{}
